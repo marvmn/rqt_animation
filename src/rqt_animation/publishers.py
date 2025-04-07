f774c29301e13fae9d1065ede574c1d5f93ab2af
Marvin Wiebe <marvin.wiebe@uni-bielefeld.de>
2025-04-07 11:21:12 +0200

    use robot state instead of fake trajectory

--- src/rqt_animation/publishers.py
+++ src/rqt_animation/publishers.py
@@ -5,12 +5,10 @@ import sys
 # moveit
 import moveit_commander
 from moveit_commander.robot import RobotCommander
-from moveit_commander.move_group import MoveGroupCommander
 
 # messages
-from moveit_msgs.msg import DisplayTrajectory, RobotTrajectory
+from moveit_msgs.msg import DisplayRobotState
 from sensor_msgs.msg import JointState
-from trajectory_msgs.msg import JointTrajectoryPoint
 
 class PublisherManager():
 
@@ -27,7 +25,7 @@ class PublisherManager():
         self.publish_real_states = False
 
         # publishers
-        self.pub_fake = rospy.Publisher("/move_group/display_planned_path", DisplayTrajectory, queue_size=20)
+        self.pub_fake = rospy.Publisher("/display_robot_state", DisplayRobotState, queue_size=10)
         self.pub_real = rospy.Publisher("/joint_command", JointState, queue_size=10)
 
     def publish_state(self, state):
@@ -38,22 +36,9 @@ class PublisherManager():
             self.pub_real.publish(state)
         # else:
 
-        # create a fake trajectory that contains the single state needed
-        display_trajectory = DisplayTrajectory()
-        display_trajectory.trajectory_start = self.robot.get_current_state()
-        display_trajectory.model_id = "panda"
-
-        fake_trajectory = RobotTrajectory()
-        fake_trajectory.joint_trajectory.header.frame_id = "panda_link0"
-        fake_trajectory.joint_trajectory.joint_names = self.robot.get_active_joint_names()
-        point = JointTrajectoryPoint()
-        point.positions = state.position
-        fake_trajectory.joint_trajectory.points.append(point)
-
-        display_trajectory.trajectory.append(fake_trajectory)
-
-        # publish!
-        self.pub_fake.publish(display_trajectory)
+        display = DisplayRobotState()
+        display.state.joint_state = state
+        self.pub_fake.publish(display)
     
     def play_from(self, time):
         """
