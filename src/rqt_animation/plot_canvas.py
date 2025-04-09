08599b45dec45e605ef27fea02038161a6ba8b77
Marvin Wiebe <marvin.wiebe@uni-bielefeld.de>
2025-04-09 19:01:48 +0200

    dragging and selecting now works well

--- src/rqt_animation/plot_canvas.py
+++ src/rqt_animation/plot_canvas.py
@@ -3,6 +3,7 @@ import matplotlib
 matplotlib.use('Qt5Agg')
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
+from matplotlib.patches import Rectangle
 import numpy as np
 import copy
 
@@ -32,6 +33,9 @@ class MplCanvas(FigureCanvasQTAgg):
 
         # rectangle that appears when dragging the mouse to select something
         self.selection_rect = None
+
+        # are these keys pressed currently?
+        self.key_ctrl = False
 
         super().__init__(self.fig)
     
@@ -75,21 +79,27 @@ class MplCanvas(FigureCanvasQTAgg):
         self.mpl_connect('button_press_event', self._on_mouse_press)
         self.mpl_connect('button_release_event', self._on_mouse_release)
         self.mpl_connect('motion_notify_event', self._on_mouse_move)
+        self.mpl_connect('key_press_event', self._on_key_press)
+        self.mpl_connect('key_release_event', self._on_key_release)
+        self.mpl_connect('axes_enter_event', self.on_enter_event)
     
     def draw_timebars(self, time):
         """
         Draw vertical lines that mark the positions of keyframes and a distinct line
-        that indicates which time is currently selected
+        that indicates which time is currently selected.
+        If time is None, the indicator is not redrawn and stays the same.
         """
         # first delete old lines
         if not self.timebars is None:
             self.timebars.remove()
-        if not self.indicator is None:
+        if not self.indicator is None and not time is None:
             self.indicator.remove()
         
         # now draw vlines
         self.timebars = self.axes.vlines(self.times, -4, 4, linestyles="dotted")
-        self.indicator = self.axes.vlines(time, -5, 5, linestyles='dashed')
+
+        if not time is None:
+            self.indicator = self.axes.vlines(time, -5, 5, linestyles='dashed')
 
         # refresh
         self.draw()
@@ -115,6 +125,11 @@ class MplCanvas(FigureCanvasQTAgg):
 
             # show selection
             self._update_selection()
+        
+        # if not, nothing was clicked on, so we deselect everything
+        else:
+            self.selected = []
+            self._update_selection()
             
 
     def _on_mouse_release(self, event):
@@ -131,9 +146,12 @@ class MplCanvas(FigureCanvasQTAgg):
 
                 self.times[i] = s._offsets[i][0]
                 self.positions[i][joint_idx] = s._offsets[i][1]
+            
+            # redraw time bars
+            self.draw_timebars(None)
 
         # check if a point is hovered and was just released
-        if not self.hovered is None:
+        if not self.hovered is None and self.hovered in self.selected:
 
             # deselect point
             self.selected.remove(self.hovered)
@@ -147,17 +165,30 @@ class MplCanvas(FigureCanvasQTAgg):
         # delete mouse grab point
         self.grabbed = None
 
+        # remove selection rect
+        if not self.selection_rect is None:
+            self.selection_rect.remove()
+            self.selection_rect = None
+            self.draw()
+
     def _on_mouse_move(self, event):
         """
         Called when the user moves their mouse
         """
 
         # first check if something is selected and the mouse button has been pressed
-        if self.selected and not self.grabbed is None:
+        # while there is no selection rect.
+        # That means that the current selection is being moved.
+        if self.selected and not self.grabbed is None and self.selection_rect is None:
 
             # something is selected and the mouse was dragged
             # move selected points the specified amount
             movement = np.array([event.xdata, event.ydata]) - self.grabbed
+
+            # if CTRL was held down, only move on the y axis
+            if self.key_ctrl:
+                movement[0] = 0.0
+
             for (s, i) in self.selected:
                 joint_idx = int(s.get_label()[len('joint'):])
                 original_position = np.array([self.times[i], self.positions[i][joint_idx]])
@@ -165,6 +196,10 @@ class MplCanvas(FigureCanvasQTAgg):
                 # move the point from it's original position
                 new_position = original_position + movement
                 s._offsets[i] = new_position
+
+                # adjust x values of all other points at this time as well
+                for si in self.scatters:
+                    si._offsets[i][0] = new_position[0]
 
                 # move lines accordingly
                 xdata = self.lines[joint_idx].get_xdata()
@@ -179,37 +214,87 @@ class MplCanvas(FigureCanvasQTAgg):
 
 
         # check if a point was hovered
-        for s in self.scatters:
-            hit, item_dict = s.contains(event)
+        if self.selection_rect is None:
+            for s in self.scatters:
+                hit, item_dict = s.contains(event)
 
-            # point found?
-            if hit:
+                # point found?
+                if hit:
 
-                # get point index
-                index = item_dict['ind'][0]
+                    # get point index
+                    index = item_dict['ind'][0]
 
-                # if this point has already been marked as hovered, skip
-                if self.hovered == (s, index):
+                    # if this point has already been marked as hovered, skip
+                    if self.hovered == (s, index):
+                        return
+                    
+                    # deselect old point if there was one different from the current
+                    if not self.hovered is None:
+                        self.hovered[0].get_sizes()[self.hovered[1]] = self.default_size
+
+                    # mark as hovered and increase size of this point
+                    self.hovered = (s, index)
+                    s.get_sizes()[index] = self.default_size * 2
+
+                    # redraw canvas
+                    self.draw()
+
                     return
-                
-                # deselect old point if there was one different from the current
-                if not self.hovered is None:
-                    self.hovered[0].get_sizes()[self.hovered[1]] = self.default_size
-
-                # mark as hovered and increase size of this point
-                self.hovered = (s, index)
-                s.get_sizes()[index] = self.default_size * 2
-
-                # redraw canvas
-                self.draw()
-
-                return
         
         # if no point was touched, deselect and unhover
         if not self.hovered is None:
             self.hovered[0].get_sizes()[self.hovered[1]] = self.default_size
             self.hovered = None
             self.draw()
+
+        # if no point was touched and the mouse is grabbing, draw selection rect
+        if self.hovered is None and not self.grabbed is None:
+
+            if not self.selection_rect is None:
+                self.selection_rect.remove()
+
+            difference = np.array([event.xdata, event.ydata]) - self.grabbed
+
+            self.selection_rect = Rectangle(self.grabbed, difference[0], difference[1],
+                                            linewidth=1, edgecolor='b', facecolor=(0.0, 0.2, 0.8, 0.2))
+            self.axes.add_patch(self.selection_rect)
+
+            # update selected points
+            self.selected = []
+            x1 = min(self.grabbed[0], event.xdata)
+            x2 = max(self.grabbed[0], event.xdata)
+            y1 = min(self.grabbed[1], event.ydata)
+            y2 = max(self.grabbed[1], event.ydata)
+
+            for scat in self.scatters:
+                for i in range(len(scat._offsets)):
+                    point = scat._offsets[i]
+
+                    if x1 < point[0] and point[0] < x2 and y1 < point[1] and point[1] < y2:
+                        self.selected.append((scat, i))
+
+            self._update_selection()
+
+    def _on_key_press(self, event):
+        """
+        Called when a key is pressed.
+        """
+
+        # update key down variables
+        if event.key == 'control':
+            self.key_ctrl = True
+    
+    def _on_key_release(self, event):
+        """
+        Called when a key is released.
+        """
+
+        # update key down variables
+        if event.key == 'control':
+            self.key_ctrl = False
+    
+    def on_enter_event(self, _):
+        self.setFocus()
 
     # ---------------- SELECTION HELPERS ----------------------
     def _update_selection(self):
