5afc860f812e08d77655252a77dde32daaa895f8
Marvin Wiebe <marvin.wiebe@uni-bielefeld.de>
2025-04-27 13:38:50 +0200

    fixed CMakeLists dependency issues

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
   
 d = generate_distutils_setup(
-    packages=['rqt_mypkg'],
+    packages=['rqt_animation'],
     package_dir={'': 'src'},
 )
 
